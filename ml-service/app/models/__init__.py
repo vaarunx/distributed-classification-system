5aa1054a7dbf433780eec054c25b4363f11fa823
vaarunx <varunrishwandh2001@gmail.com>
2025-11-08 13:28:02 -0500

    Adding ml-service

---
+++ ml-service/app/models/__init__.py
@@ -0,0 +1,5 @@
+from .mobilenet_model import MobileNetModel
+from .clip_model import ClipModel
+from .base_model import BaseClassificationModel
+
+__all__ = ["MobileNetModel", "ClipModel", "BaseClassificationModel"]
---
+++ ml-service/app/schemas/__init__.py
@@ -0,0 +1,17 @@
+from .requests import (
+    ClassificationRequest,
+    ClassificationResponse,
+    ImageResult,
+    ClassificationSummary,
+    Prediction,
+    JobType
+)
+
+__all__ = [
+    "ClassificationRequest",
+    "ClassificationResponse",
+    "ImageResult",
+    "ClassificationSummary",
+    "Prediction",
+    "JobType"
+]
